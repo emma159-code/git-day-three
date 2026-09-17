87f5473b7c45f52ed1b4b8628cb3584a652a5659
emma159-code <akandaemmanuella9@icloud.com>
2026-09-17 17:30:35 +0200

    delete one line

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 console.log("My name is Emmanuella Akanda.");
 console.log("I am a Software Engineering student.");
 console.log("I am from Gabon and I study in Rwanda.");
-console.log("I want to become a skilled software developer.");
+
